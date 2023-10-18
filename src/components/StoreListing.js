ae7c3972426596e0d145b45270dabfd4c6a3d09d
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2023-10-17 22:43:32 -0400

    bootstrap link

---
+++ src/components/StoreListing.js
@@ -0,0 +1,33 @@
+//<h2>Browse stores in Houston</h2>
+<Container style={{ marginTop: "40px", textAlign: "center" }}>
+    <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} >
+        <Row>
+            <Col xs=".5" >
+                <Image src="[IMG SRC]" roundedCircle />
+            </Col>
+            <Col>
+                <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+                <p>Delivery * Pickup</p>
+            </Col>
+        </Row>
+        <Col sm="1"></Col>
+        <Row>
+            <Col xs=".5">
+                <Image src="[IMG SRC]" roundedCircle />
+            </Col>
+            <Col>
+                <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
+                <p>Delivery * Pickup</p>
+            </Col>
+        </Row>
+        <Col sm="1"></Col>
+        <Row >
+            <Col xs=".5" >
+                <Image src="[IMG SRC]" roundedCircle />
+            </Col>
+            <Col md="6">
+                <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
+                <p>Delivery</p>
+            </Col>
+        </Row>
+    </Row>//</Container>
---
+++ src/components/TopBar.js
@@ -0,0 +1,14 @@
+<Navbar bg="light" expand="lg" >
+    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+        <img src="[Instacart logo src]" /> 
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+        </Nav>
+        <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{' '}
+        </Nav>
+    </Navbar.Collapse>
+</Navbar>
